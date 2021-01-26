3d3fd9d0a824990f8f947c7d6c1e4c748e56853e
arthurgehrke <arthurgehrke07@gmail.com>
2021-01-26 16:58:49 -0300

    feat: add user model

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace library_system.Models
+{
+  public class User
+  {
+    public Guid Id { get; init; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+  }
+}
